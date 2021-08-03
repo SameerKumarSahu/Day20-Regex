86ef0b01cff9a4280eb417b9e9a91d10a339eb92
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:32:50 +0530

    Check Phone Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,20 +4,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        //UC3
-        /*E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) withprecise @ and . positions*/
-
-        String regex3=("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*");
-        System.out.println("Enter your mail address :");
+        //UC4
+        /*Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number*/
+        String regex4="^(0|91)?[ ][0-9]{10}$";
+        System.out.println("Enter your phone number :");
         Scanner sc = new Scanner(System.in);
-        String eMail = sc.nextLine();
-        Pattern p3=Pattern.compile(regex3);
-        Matcher m3= p3.matcher(eMail);
-        boolean result3 =m3.matches();
-        if (result3==true) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("please enter a Valid mail adress");
-        }
+        String phoneNum = sc.nextLine();
+        Pattern p4=Pattern.compile(regex4);
+        Matcher m4= p4.matcher(phoneNum);
+        boolean result4 =m4.matches();
+        if (result4==true)  System.out.println("Valid");
+        else System.out.println("please enter a Valid phone number");
     }
 }
