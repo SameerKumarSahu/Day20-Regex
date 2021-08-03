82554b742f39c36cfbcaaeba3b1587def0b3b8a4
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:01:30 +0530

    Check First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,5 +5,19 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Programme");
+        //UC1
+        /*First name starts with Cap and has minimum 3 characters*/
+        System.out.println("Enter your FirstName :");
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.nextLine();
+        String regex1 = "^([A-Z]{1}+[a-z]{2,})$";
+        Pattern p1 = Pattern.compile(regex1);
+        Matcher m1 = p1.matcher(firstName);
+        boolean result1 = m1.matches();
+        if (result1 == true) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("please enter a Valid name");
+        }
     }
 }
