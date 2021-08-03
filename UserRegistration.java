3325c13b537ad410fa32d50c117cfb1dba8f8553
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:44:07 +0530

    Rule-3 for PassWord

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,16 +4,17 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        //UC6
-        /*Rule2– Should have at least 1 Upper Case */
-        String regex6="^([A-Z]{1}[a-zA-Z0-9@!&$%#-_*+]{7,})";
+        //UC7
+        /*Rule3 – Should have at least 1 numeric number in the password*/
+        String regex7="^([A-Z]{1}[0-9]{1}[a-zA-Z0-9@!&$%#-_*+]{6,})";
         System.out.println("Enter your password: ");
         Scanner sc = new Scanner(System.in);
-        String passWord2 = sc.next();
-        Pattern p6=Pattern.compile(regex6);
-        Matcher m6= p6.matcher(passWord2);
-        boolean result6 =m6.matches();
-        if (result6==true)  System.out.println("Valid");
+        String passWord3 = sc.next();
+        Pattern p7=Pattern.compile(regex7);
+        Matcher m7= p7.matcher(passWord3);
+        boolean result7 =m7.matches();
+        if (result7==true)  System.out.println("Valid");
         else System.out.println("please enter a Valid password");
+
     }
 }
