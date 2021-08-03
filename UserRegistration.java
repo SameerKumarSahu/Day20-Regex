77738144dcefa22eff717468c705700854ec3438
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:09:13 +0530

    Check Mail address

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,19 +4,20 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Programme");
-        // UC2
-        /* Last name starts with Cap and has minimum 3 characters*/
-        System.out.println("Enter your LastName :");
-        String lastName = sc.nextLine();
-        String regex2 = "^([A-Z]{1}+[a-z]{2,})$";
-        Pattern p2 = Pattern.compile(regex2);
-        Matcher m2 = p2.matcher(lastName);
-        boolean result2 = m2.matches();
-        if (result2 == true) {
+        //UC3
+        /*E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) withprecise @ and . positions*/
+
+        String regex3=("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*");
+        System.out.println("Enter your mail address :");
+        Scanner sc = new Scanner(System.in);
+        String eMail = sc.nextLine();
+        Pattern p3=Pattern.compile(regex3);
+        Matcher m3= p3.matcher(eMail);
+        boolean result3 =m3.matches();
+        if (result3==true) {
             System.out.println("Valid");
         } else {
-            System.out.println("please enter a Valid name");
+            System.out.println("please enter a Valid mail adress");
         }
     }
 }
