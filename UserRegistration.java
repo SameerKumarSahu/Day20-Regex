c86befc178c03dd0e319ad1d09e840fc5029975e
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:36:01 +0530

    Rule-1 (Password)

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,16 +4,16 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static void main(String[] args) {
-        //UC4
-        /*Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number*/
-        String regex4="^(0|91)?[ ][0-9]{10}$";
-        System.out.println("Enter your phone number :");
+        //UC5
+        /*Rule1-minimum 8 Characters*/
+        String regex5="^[A-Za-z0-9@!&$%#-_*+]{8,}";
+        System.out.println("Enter your password: ");
         Scanner sc = new Scanner(System.in);
-        String phoneNum = sc.nextLine();
-        Pattern p4=Pattern.compile(regex4);
-        Matcher m4= p4.matcher(phoneNum);
-        boolean result4 =m4.matches();
-        if (result4==true)  System.out.println("Valid");
-        else System.out.println("please enter a Valid phone number");
+        String passWord1 = sc.next();
+        Pattern p5=Pattern.compile(regex5);
+        Matcher m5= p5.matcher(passWord1);
+        boolean result5 =m5.matches();
+        if (result5==true)  System.out.println("Valid");
+        else System.out.println("please enter a Valid password");
     }
 }
