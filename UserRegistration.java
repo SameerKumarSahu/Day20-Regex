b45f92a01fdb1f078dcc762a6cfad985fcc154c2
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:03:52 +0530

    Check Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -19,5 +19,19 @@ public class UserRegistration {
         } else {
             System.out.println("please enter a Valid name");
         }
+
+        // UC2
+        /* Last name starts with Cap and has minimum 3 characters*/
+        System.out.println("Enter your LastName :");
+        String lastName = sc.nextLine();
+        String regex2 = "^([A-Z]{1}+[a-z]{2,})$";
+        Pattern p2 = Pattern.compile(regex2);
+        Matcher m2 = p2.matcher(lastName);
+        boolean result2 =m2.matches();
+        if (result2==true) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("please enter a Valid name");
+        }
     }
 }
