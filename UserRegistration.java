705f053922eec10605e76a6c8e285fd2ac9dc801
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-03 07:04:44 +0530

    Check Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,21 +5,6 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Programme");
-        //UC1
-        /*First name starts with Cap and has minimum 3 characters*/
-        System.out.println("Enter your FirstName :");
-        Scanner sc = new Scanner(System.in);
-        String firstName = sc.nextLine();
-        String regex1 = "^([A-Z]{1}+[a-z]{2,})$";
-        Pattern p1 = Pattern.compile(regex1);
-        Matcher m1 = p1.matcher(firstName);
-        boolean result1 = m1.matches();
-        if (result1 == true) {
-            System.out.println("Valid");
-        } else {
-            System.out.println("please enter a Valid name");
-        }
-
         // UC2
         /* Last name starts with Cap and has minimum 3 characters*/
         System.out.println("Enter your LastName :");
@@ -27,8 +12,8 @@ public class UserRegistration {
         String regex2 = "^([A-Z]{1}+[a-z]{2,})$";
         Pattern p2 = Pattern.compile(regex2);
         Matcher m2 = p2.matcher(lastName);
-        boolean result2 =m2.matches();
-        if (result2==true) {
+        boolean result2 = m2.matches();
+        if (result2 == true) {
             System.out.println("Valid");
         } else {
             System.out.println("please enter a Valid name");
