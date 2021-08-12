0a4082e4bfb838d7210aab37c7f033ee71011e25
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-12 19:10:32 +0530

    Refactor the code using Lambda function

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,59 +1,49 @@
-package org.example;
+package com.Bridgelabz;
 
-import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        UserRegistration user = new UserRegistration();
-        System.out.println("Welcome to User Registration Program");
-    }
-    //Firstname
-    public boolean firstNameCheck(String str) {
-        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", str);
-        if (check)
-            System.out.println("Valid FirstName ");
-        else
-            System.out.println("Please Enter a Valid First name with staring letter Capital");
-        return check;
-    }
-    //Lastname
-    public boolean lastNameCheck(String str) {
-        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", str);
-        if (check)
-            System.out.println("Valid LastName");
-        else
-            System.out.println("Please Enter a Valid Last Name with staring letter Capital");
-        return check;
-    }
-    //Email
-    public boolean email(String str) {
-        boolean check = Pattern.matches("^[a-z]*([.]?[a-z]+)*@bl[.]co([.]?in)*", str);
-        if (check)
-            System.out.println("Valid Email address");
-        else
-            System.out.println("Please Enter a Valid Email address as given in example xyz.abc@bl.co.in");
-        return check;
-    }
-    //Phone Number
-    public boolean mobileNum(String str) {
-        boolean check = Pattern.matches("^91\\s[6789][0-9]{9}", str);
-        if (check)
-            System.out.println("Valid MobileNumber ");
-        else
-            System.out.println("Please Enter a Valid Mobile Number e.g 91 8965471234");
-        return check;
-    }
-        // Password should have minimum 8 characters with, 1 upper case, 1 Numeric and 1 Special Char
-    public boolean passWord(String str) {
-        boolean check = Pattern.matches("[A-Z]{1}[a-z]{5,}[0-9]{1}[!@#$%^&*~]{1}", str);
-        if (check)
-            System.out.println("Valid Password");
-        else
-            System.out.println("Please Enter a Valid password and it should have minimum 8 characters with, 1 upper case, 1 Numeric and 1 Special Char ");
-        return check;
-    }
 
+	public static void main(String[] args) {
+		 String FIRST_NAME_PATTERN = "[A-Z]{1}[a-zA-Z]{2,}";
+		 String LAST_NAME_PATTERN = "[A-Z]{1}[a-zA-Z]{2,}";
+		 String EMAIL_ID_PATTERN = "^[a-zA-Z]+[-+.]{0,1}[a-zA-Z0-9]+@[A-Za-z0-9]+(.[A-Za-z]{2,4}){1,2}$";
+		 String MOBILE_NUMBER_PATTERN = "[1-9]{1}[0-9]{1}( )[1-9]{1}[0-9]{9}";
+		 String PASSWORD_PATTERN = "(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])[a-zA].{8,}$";
 
+		Predicate<String> validFirstName = (String firstName) -> {
+			Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+			boolean result = pattern.matcher(firstName).matches();
+			return result;
+		};
+
+		Predicate<String> validLastName = (String lastName) -> {
+			boolean result = Pattern.matches(LAST_NAME_PATTERN, lastName);
+			return result;
+		};
+
+		Predicate<String> validEmailId = (String emailId) -> {
+			boolean result = Pattern.matches(EMAIL_ID_PATTERN, emailId);
+			return result;
+
+		};
+
+		Predicate<String> validMobileNumber = (String mobileNumber) -> {
+			boolean result = Pattern.matches(MOBILE_NUMBER_PATTERN, mobileNumber);
+			return result;
+		};
+
+		Predicate<String> validPassword = (String password) -> {
+			boolean result = Pattern.matches(PASSWORD_PATTERN, password);
+			return result;
+		};
+
+		System.out.println(validFirstName.test("Sajaya"));
+		System.out.println(validLastName.test("Singh"));
+		System.out.println(validEmailId.test("singh@gmail.com"));
+		System.out.println(validMobileNumber.test("91 9874556321"));
+		System.out.println(validPassword.test("Akdfjk12@"));
+
+	}
 }
